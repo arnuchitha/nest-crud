54d9257a754df8b13e98d85ab9ab60351f02573b
Anuchit.C <Ha.shownoob@gmail.com>
2022-06-03 13:18:07 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity';
+ 
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: null,
+      database: 'nestjs',
+      entities: [User],
+      // entities: ['../typeorm/entities/*.ts'],
+      synchronize: true,
+    }), ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
